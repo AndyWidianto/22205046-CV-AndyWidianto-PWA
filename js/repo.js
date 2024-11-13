89f512fd8f7968e76cf0c4df9454c92ea1d46a83
AndyWidianto <gamestory5001@gmail.com>
2024-11-14 05:33:03 +0700

    first commit

--- js/repo.js
+++ js/repo.js
@@ -17,7 +17,7 @@ fetch("repos.json")
         repo.url
       }"class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect bg-gray mdl-shadow--8dp"data-upgraded="MaterialButton,MaterialRipple"><i class="zmdi zmdi-github-alt"></i><span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a></div><div class="mdl-card__actions mdl-card--border"><span class="blog-post-date inline-block">${
         repo.date.split("T")[0]
-      }</span><div class="mdl-layout-spacer"></div><div class="mdl-card__actions"><img class="mr-5"src="img/star.svg"/><span>${
+      }</span><div class="mdl-layout-spacer"></div><div class="mdl-card__actions"><img class="mr-5"src="img/icon-andy.jpg"/><span>${
         repo.stars
       }</span><img class="mr-5 ml-5"src="img/fork.svg"/><span>${
         repo.forks
@@ -26,24 +26,6 @@ fetch("repos.json")
     document.getElementById("repo-card").innerHTML = output;
   });
 
-const request = indexedDB.open("ContactDB", 1);
-
-request.onerror = function(event) {
-    console.error("Database error:", event.target.errorCode);
-};
-
-request.onsuccess = function(event) {
-    console.log("Database initialized");
-};
-
-request.onupgradeneeded = function(event) {
-    const db = request.result;
-    const objectStore = db.createObjectStore("contacts", { keyPath: "id", autoIncrement: true });
-    objectStore.createIndex("name", ["name"], { unique: false });
-    objectStore.createIndex("email", ["name","email"], { unique: false });
-    objectStore.createIndex("message", ["name","email","message"], { unique: false });
-    console.log("Object Store created");
-};
 
 function requestNotificationPermission() {
     if (Notification.permission === "default") {
@@ -59,32 +41,57 @@ function showNotification() {
   if (Notification.permission === "granted") {
       new Notification("Pesan Terkirim", {
           body: "Data berhasil disimpan di IndexedDB",
-          icon: "/assets/img/php.png"
+          icon: "/img/icon-144x144.png"
       });
   }
 }
-request.onsuccess = function(name, email, message){
-  console.log("Database berhasil disimpan");
-  const db = request.result;
-
-  const transaction = db.transaction("contact", "readwrite");
-
-  const store = transaction.objectStore("contact");
-
-  store.add(name, email, message);
-  store.put({ name: "andy", email: "andy@gmail.com", message: "hihihihihih"});
-
-}
-document.getElementById("contact").addEventListener("submit", function(event) {
+document.getElementById("contact").addEventListener("click", function(event) {
   event.preventDefault();
 
+  const request = window.indexedDB.open("DatabaseContacts", 1);
+
+
+  // menyimpan sebuah value
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const message = document.getElementById("message").value;
 
-  request.onsuccess(name, email, message);
+  request.onerror = (event)=>{
+    console.error("IndexedDB tidak tersambung");
+    console.error(event)
+  }
 
-  document.getElementById("contact").reset();
+  request.onupgradeneeded = ()=>{
+    const db = request.result;
+    
+    const store = db.createObjectStore("Contact", {keyPath: "id" ,autoIncrement: true});
+    store.createIndex("name", ["name"], {unique: false});
+    store.createIndex("email" ,["email"], {unique: false});
+    store.createIndex("message", ["message"],{unique: false});
+  }
+
+  request.onsuccess = function (){
+    console.log("Database berhasil disimpan");
+
+    const db = request.result;
+
+    const transaction = db.transaction("Contact", "readwrite");
+    const store = transaction.objectStore("Contact");
+    console.log(name+ email+ message);
+
+    const data = {
+      name: name,
+      email: email,
+      message: message
+    }
+    store.add(data);
+    const idQuery = store.get();
+    console.log(idQuery);
+
+    transaction.oncomplete = ()=>{
+      db.close();
+    }
+  }
 });
 
 document.addEventListener("DOMContentLoaded", requestNotificationPermission);
